17567a3b999cb1a51b29321aefb96a07050f4caa
Rico Mitchell <ricomitch@icloud.com>
2019-11-14 02:11:13 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -11,5 +11,25 @@ class Transfer
   def valid?
     sender.valid? && receiver.valid?
   end
+  
+  def execute_transaction
+    if valid? && sender.balance > amount && status == "pending"
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
+    else 
+      self.status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+  end
+    
+    def reverse_transfer
+      if self.status == "complete"
+        self.status = "reversed"
+        sender.balance += amount
+      receiver.balance -= amount
+    end
+    end
+  
+  end
 
 end
