5f3f6082b8aec634e1877fccac60c6dab215a13f
Rico Mitchell <ricomitch@icloud.com>
2019-11-14 02:16:59 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,15 +1,22 @@
 class Transfer
+ class Transfer
   attr_accessor :sender, :receiver, :amount, :status
-
-  def initialize(sender, receiver, status)
-    @sender = sender
+  
+  def initialize(sender, receiver, amount)
     @receiver = receiver
+    @sender = sender 
+    @amount = amount
     @status = "pending"
-    @amount = 50
   end
-
-  def valid?
+  
+  def valid? 
+    # binding.pry
+    
     sender.valid? && receiver.valid?
+    
+    #valid if both accounts status == "open" and balance > 0 
+    #should reject transfer if accounts aren't valid.. or if balance <=0
+  
   end
   
   def execute_transaction
@@ -31,5 +38,47 @@ class Transfer
     end
   
   end
-
+  
+  
+endclass Transfer
+  attr_accessor :sender, :receiver, :amount, :status
+  
+  def initialize(sender, receiver, amount)
+    @receiver = receiver
+    @sender = sender 
+    @amount = amount
+    @status = "pending"
+  end
+  
+  def valid? 
+    # binding.pry
+    
+    sender.valid? && receiver.valid?
+    
+    #valid if both accounts status == "open" and balance > 0 
+    #should reject transfer if accounts aren't valid.. or if balance <=0
+  
+  end
+  
+  def execute_transaction
+    if valid? && sender.balance > amount && status == "pending"
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
+    else 
+      self.status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+  end
+    
+    def reverse_transfer
+      if self.status == "complete"
+        self.status = "reversed"
+        sender.balance += amount
+      receiver.balance -= amount
+    end
+    end
+  
+  end
+  
+  
 end
