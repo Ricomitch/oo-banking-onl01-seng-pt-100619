09cd3c5fb8bb6f1f227c4ff0ce07bcba34a229fc
Rico Mitchell <ricomitch@icloud.com>
2019-11-14 01:57:58 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,33 @@
+require 'pry'
+
 class BankAccount
+  attr_reader :name,:balance, :status
+
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(amount)
+    @balance += amount
+
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    status == "open" && balance > 0
+  end
+
+  def close_account
+    @status = "closed"
+    #binding.pry
+  end
+
+
 
 end
